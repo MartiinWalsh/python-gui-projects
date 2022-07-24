3a82da120a48c405696a461b1ea323c0689fc0e1
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-24 20:33:27 +0100

    Button and Entry added

--- unit-converter/main.py
+++ unit-converter/main.py
@@ -1,7 +1,28 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI Program")
 window.minsize(width=500, height=300)
 
+
+# Label
+
+my_label = Label(text="Unit converter", font=("Arial", 24, "bold"))
+my_label.pack()
+
+# Button
+
+def button_listener():
+    my_label.config(text=input.get())
+
+button = Button(text="Click me", command=button_listener)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+
 window.mainloop()
+
+

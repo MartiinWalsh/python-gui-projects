49fde39a8ce7c991ae8453ef08d84270c2c86b10
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-29 22:29:42 +0100

    Added countdown mechanism

--- pomodoro-app/main.py
+++ pomodoro-app/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,12 +9,42 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global REPS
+    REPS += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif REPS % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02}")
+    if count > 0:
+        window.after(1000, count_down, count -1)
+    else:
+        start_timer()
+        global REPS
+        mark = ""
+        if REPS % 2 == 0:
+            count_label.config(text="✔")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,16 +57,17 @@ timer_label.grid(row=0, column=1)
 background_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=background_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW)
+
+start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW)
 reset_button.grid(row=2, column=2)
 
-count_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+count_label = Label(fg=GREEN, bg=YELLOW)
 count_label.grid(row=3, column=1)
 
 window.mainloop()

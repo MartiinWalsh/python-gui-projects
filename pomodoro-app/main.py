6478f776edabb85492e7ce7a18f133ad54663397
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-31 08:35:58 +0100

    Timer reset

--- pomodoro-app/main.py
+++ pomodoro-app/main.py
@@ -9,22 +9,29 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-REPS = 0
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, test = "00:00")
+    timer_label.config(text="Timer")
+    count_label.config(text="")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    global REPS
-    REPS += 1
+    global reps
+    reps += 1
 
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
-    if REPS % 8 == 0:
+    if reps % 8 == 0:
         count_down(long_break_sec)
         timer_label.config(text="Break", fg=RED)
-    elif REPS % 2 == 0:
+    elif reps % 2 == 0:
         count_down(short_break_sec)
         timer_label.config(text="Break", fg=PINK)
     else:
@@ -38,13 +45,16 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
     else:
         start_timer()
-        global REPS
-        mark = ""
-        if REPS % 2 == 0:
-            count_label.config(text="✔")
+        global reps
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        count_label.config(text = marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -64,7 +74,7 @@ canvas.grid(row=1, column=1)
 start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW)
+reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW, command=reset)
 reset_button.grid(row=2, column=2)
 
 count_label = Label(fg=GREEN, bg=YELLOW)
